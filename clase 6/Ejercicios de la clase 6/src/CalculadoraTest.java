2f964eb9b019c2f4f28dcc0d89361291f49fd2e2
Claudio-Olivera <claudioolivera753@gmail.com>
2023-03-16 13:37:22 -0300

    Terminado ejercicio clase 6, Add de pdfs

---
+++ clase 6/Ejercicios de la clase 6/src/CalculadoraTest.java
@@ -0,0 +1,48 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+/**
+ * Implemente una clase “Calculadora” que tenga cuatro métodos, uno por cada operación
+ * aritmética básica (suma, resta, multiplicación y división), respetando las siguientes firmas:
+ * ● double sumar(double unNumero, double otroNumero);
+ * ● double restar(double unNumero, double otroNumero);
+ * ● double multiplicar(double unNumero, double otroNumero);
+ * ● double dividir(double unNumero, double otroNumero);
+ * Luego de haber implementado la Calculadora, realice los siguientes casos de prueba:
+ * 1. El resultado de multiplicar 80 por 3 da 240
+ * 2. El resultado de sumar 150 y 180, dividido por 3, da 110
+ * 3. El resultado de restar 90 y 50, multiplicado por 15, no da 605.
+ * 4. El resultado de sumar 70 y 40, multiplicado por 25, no da 2700.
+ */
+public class CalculadoraTest {
+    private Calculadora calculadora;
+    @BeforeEach
+    //De esta forma usando @BeforeEach creo una nueva instancia de la clase para cada @Test que realizo.
+    public void setUp() {
+        calculadora = new Calculadora();
+    }
+    @Test
+    public void testOne() {
+        double resultado = calculadora.multiplicar(80, 3);
+        Assert.assertEquals(240, resultado, 0);
+    }
+
+    @Test
+    public void testTwo() {
+        double resultado = calculadora.dividir(Calculadora.sumar(150, 180), 3);
+        Assert.assertEquals(110, resultado, 0);
+    }
+
+    @Test
+    public void testThree() {
+        double resultado = calculadora.multiplicar(Calculadora.restar(90, 50), 15);
+        Assert.assertEquals(605, resultado, 0);
+    }
+
+    @Test
+    public void testFour() {
+        double resultado = calculadora.multiplicar(Calculadora.sumar(70, 40), 25);
+        Assert.assertEquals(2700, resultado, 0);
+    }
+}
---
+++ clase 6/Ejercicios de la clase 6/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //Simplemente, a modo de muestra de que la calculadora funciona correctamente. Las pruebas están siendo realizadas en la clase CalculadoraTest.
+
+        Calculadora calc = new Calculadora();
+        System.out.println("El resultado de sumar 20 + 30 = " + calc.sumar(20, 30));
+    }
+}
